46da9aac58ab064e74de4a559cbb550e9b055ca2
alikaiyr <alikajyr@gmail.com>
2023-12-25 21:55:27 +0600

    lazyrow instead of lazycolumn

--- app/src/main/java/com/example/forgitwork/MainActivity.kt
+++ app/src/main/java/com/example/forgitwork/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -57,7 +58,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Text(text = "Click that sh..")
         }
 
-        LazyColumn {
+        LazyRow {
             items(100) { item ->
                 // Each item in the LazyColumn is a Text composable
                 Text(
@@ -65,7 +66,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(16.dp)
-                        .background(Color(Random.nextInt(), Random.nextInt(), Random.nextInt()), RoundedCornerShape(15.dp))
+                        .background(
+                            Color(Random.nextInt(), Random.nextInt(), Random.nextInt()),
+                            RoundedCornerShape(15.dp)
+                        )
                         .padding(8.dp),
                     color = Color.White,
                 )
