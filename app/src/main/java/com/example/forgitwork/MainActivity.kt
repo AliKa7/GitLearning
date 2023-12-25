5c29a4f054b3cf96c520dcf3ed7e01a30adae043
alikaiyr <alikajyr@gmail.com>
2023-12-25 12:26:58 +0600

    added lazy column onto Greeting

--- app/src/main/java/com/example/forgitwork/MainActivity.kt
+++ app/src/main/java/com/example/forgitwork/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.forgitwork.ui.theme.ForGitWorkTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,9 +51,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(
             text = "Hello $name!",
         )
-        Button(onClick = {}, modifier = Modifier.height(50.dp).width(150.dp)) {
+        Button(onClick = {}, modifier = Modifier
+            .height(50.dp)
+            .width(150.dp)) {
             Text(text = "Click that sh..")
         }
+
+        LazyColumn {
+            items(20) { item ->
+                // Each item in the LazyColumn is a Text composable
+                Text(
+                    text = "Item $item",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .background(Color(Random.nextInt(), Random.nextInt(), Random.nextInt()), RoundedCornerShape(15.dp))
+                        .padding(8.dp),
+                    color = Color.White,
+                )
+            }
+        }
+
     }
 
 }
