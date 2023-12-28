e7f5acb80a617afcfa5bdcae47b5fd01524e57a8
alikaiyr <alikajyr@gmail.com>
2023-12-28 20:22:35 +0600

    m3

--- app/src/main/java/com/example/forgitwork/MainActivity.kt
+++ app/src/main/java/com/example/forgitwork/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Another greeting")
                     //m2
+                    //m3
                 }
             }
         }
