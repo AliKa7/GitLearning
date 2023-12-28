d31aa0aee8d1eda4f32f9b9020e6d172bf0b25fe
alikaiyr <alikajyr@gmail.com>
2023-12-28 21:05:01 +0600

    f123

--- app/src/main/java/com/example/forgitwork/MainActivity.kt
+++ app/src/main/java/com/example/forgitwork/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.forgitwork.ui.theme.ForGitWorkTheme
@@ -60,6 +61,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             .height(50.dp)
             .width(150.dp)) {
             Text(text = "Click that sh..")
+        }
+        LazyColumn(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(15.dp)) {
+            items(100) {
+                Text(text = "item of number ${it.toString()}",
+                    fontWeight = FontWeight.SemiBold)
+            }
         }
 
     }
