da8b0894c056fd4450fb3c769d2fcf948ccc6a23
alikaiyr <alikajyr@gmail.com>
2023-12-28 20:39:44 +0600

    what

--- app/src/main/java/com/example/forgitwork/MainActivity.kt
+++ app/src/main/java/com/example/forgitwork/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.forgitwork
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,9 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.forgitwork.ui.theme.ForGitWorkTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,9 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Another greeting")
-                    //m2
-                    //m3
+                    Greeting("Another greeting", modifier = Modifier.background(Color.Black))
                 }
             }
         }
@@ -41,9 +45,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-    )
+    Column(modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Hello $name!",
+        )
+        Text(
+            text = "Hello $name again!",
+        )
+        Button(onClick = {}, modifier = Modifier
+            .height(50.dp)
+            .width(150.dp)) {
+            Text(text = "Click that sh..")
+        }
+
+        LazyRow {
+            items(120) { textItem ->
+                // Each item in the LazyColumn is a Text composable
+                Text(
+                    text = "Item $textItem",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .background(
+                            Color(Random.nextInt(), Random.nextInt(), Random.nextInt()),
+                            RoundedCornerShape(15.dp)
+                        )
+                        .padding(8.dp),
+                    color = Color.White,
+                )
+            }
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
